898a4e88b3d74729a31d1a27b3ab0f3b157525ee
williamj1230 <47767870+williamj1230@users.noreply.github.com>
2019-02-20 11:55:18 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_jeffWilliams.py
@@ -0,0 +1,8 @@
+# Convert Pounds to Kilograms
+# 18 Feb 2019
+# CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Jeff Williams
+#
+Pounds = float(input(" Please enter the number of Pounds to be converted. "))
+Kilograms = Pounds /2.2046
+print ( "The number of Kilograms is  ", format (Kilograms, '.2f'))
